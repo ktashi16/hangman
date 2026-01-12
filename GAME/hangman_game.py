35f9f32d0e1af1d9b5cfd01629790512acf53ab6
karma gurung <ktashi16@gmail.com>
2026-01-12 11:41:05 -0500

    input validation for word guess

--- GAME/hangman_game.py
+++ GAME/hangman_game.py
@@ -5,7 +5,12 @@ class HangmanGame:
     def __init__(self, movies):
         self.word = random.choice(movies).upper()  #selecting a random word from movies.json
         self.masked_word = self._mask_word()
-        
+        self.guessed_letters = set()
+        self.incorrect_guesses = 0
+        self.won = False
+        self.lost = False
+
+     
     def _mask_word(self):
         masked = ""
         for char in self.word:
@@ -15,10 +20,16 @@ class HangmanGame:
                 masked += "_"
         return masked
 
+    def guess_letter(self, letter):
+        if len(letter) != 1 or not letter.isalpha():
+            raise ValueError("Guess must be a single letter")
+
+        letter = letter.upper()
 
 
 if __name__ == "__main__":
     movies = ["Inception", "Interstellar"]
     game = HangmanGame(movies)
-    print("Chosen word:", game.word)
-    print("Masked:", game.masked_word)
+    game.guess_letter("AB")
+
+
