f266c213b686ae872417d8c1e61ee9941a091da2
karma gurung <ktashi16@gmail.com>
2026-01-12 13:57:08 -0500

    Updated menu to add update and delete

--- GAME/admin.py
+++ GAME/admin.py
@@ -1,41 +1,57 @@
 from movie_repository import MovieRepository
 
 
+def print_admin_menu():
+    print("\n🎬 Movie Admin Menu")
+    print("1. Add movie")
+    print("2. View movies")
+    print("3. Update movie")
+    print("4. Delete movie")
+    print("5. Exit")
+
+
+def handle_admin_choice(choice, repo):
+    if choice == "1":
+        repo.create_movie(input("Movie title: "))
+        print("Movie added")
+
+    elif choice == "2":
+        for movie in repo.read_movies():
+            print("-", movie)
+
+    elif choice == "3":
+        repo.update_movie(
+            input("Old title: "),
+            input("New title: ")
+        )
+        print("Movie updated")
+
+    elif choice == "4":
+        repo.delete_movie(input("Movie title: "))
+        print("Movie deleted")
+
+    elif choice == "5":
+        return False
+
+    else:
+        print("Invalid option")
+
+    return True
+
+
 def movie_admin_menu():
-    while True:
-        print("\n🎬 Movie Admin Menu")
-        print("1. Add movie")
-        print("2. View movies")
-        print("3. Update movie")
-        print("4. Delete movie")
-        print("5. Exit")
+    repo = MovieRepository()
+    running = True
 
-        repo = MovieRepository()
+    while running:
+        print_admin_menu()
+        choice = input("Choose: ")
 
-        choice = input("Choose an option: ")
-
-
-        if choice == "1":
-            title = input("Movie title: ")
-            repo.create_movie(title)
-            print(f"Movie added: {title}")
-
-        elif choice == "2":
-            movies = repo.read_movies()
-            print("\nMovies:")
-            for movie in movies:
-                print("-", movie)
-
-        elif choice == "3":
-            print("Updating")
-        
-        elif choice == "4":
-            print("Deleting")
-                
-        elif choice == "5":
-            break
+        try:
+            running = handle_admin_choice(choice, repo)
+        except ValueError as e:
+            print("Error", e)
 
 
 if __name__ == "__main__":
-    repo = MovieRepository()
-    print("Admin file connected to repository")
+    movie_admin_menu()
