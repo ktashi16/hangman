71789f6391be1291ec2063e31ee29844688646c2
karma gurung <ktashi16@gmail.com>
2026-01-12 14:18:51 -0500

    Word guess method update

--- GAME/hangman_game.py
+++ GAME/hangman_game.py
@@ -4,13 +4,15 @@ import string
 class HangmanGame:
 
 
-    def __init__(self, movies):
-        self.word = random.choice(movies).upper()  #selecting a random word from movies.json
+    def __init__(self, movies, max_guesses=7):
+        self.word = random.choice(movies).upper()
         self.masked_word = self._mask_word()
         self.guessed_letters = set()
         self.incorrect_guesses = 0
         self.won = False
         self.lost = False
+        self.max_guesses = max_guesses  # added for consistency
+
         
     def _mask_word(self):
         masked = ""
@@ -22,42 +24,51 @@ class HangmanGame:
         return masked
 
     def guess_letter(self, letter):
-        MAX_GUESSES = 7
         if len(letter) != 1 or not letter.isalpha():
             raise ValueError("Guess must be a single letter")
         
+        letter = letter.upper()
         if letter in self.guessed_letters:
             raise ValueError("Letter already guessed")
 
-        letter = letter.upper()
+        self.guessed_letters.add(letter)
 
         if letter in self.word:
             self._reveal_letter(letter)
         else:
             self.incorrect_guesses += 1
 
-        if self.incorrect_guesses >= MAX_GUESSES:
+        if self.incorrect_guesses >= self.max_guesses:
             self.lost = True
-            self.masked_word = self.word
+            self.reveal_word()
 
         if self.masked_word == self.word:
             self.won = True
 
+
     def guess_word(self, guessed_word):
         guessed_word = guessed_word.upper()
-        if self.is_over():  # game already ended
+        if self.is_over():  # ignore input if game is over
             return
+
         if guessed_word == self.word:
             self.won = True
-            self.reveal_word()  # show the full word to the player
+            self.reveal_word()  # show full word
         else:
             self.incorrect_guesses += 1
-            if self.incorrect_guesses >= self.max_incorrect_guesses:
+            if self.incorrect_guesses >= self.max_guesses:
                 self.lost = True
-                self.reveal_word()  # show the full word after losing
+                self.reveal_word()  # show full word if losing
+
+    def is_over(self):
+        return self.won or self.lost
+
+
 
     def reveal_word(self):
         self.masked_word = self.word
+
+
 
 
     def _reveal_letter(self, letter):
