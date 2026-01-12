78c8aae682a9cc3636e10dc573737da8fad8a63b
karma gurung <ktashi16@gmail.com>
2026-01-12 10:59:09 -0500

    Check file path exists

--- movies/movie_repository.py
+++ movies/movie_repository.py
@@ -5,6 +5,14 @@ import json
 class MovieRepository:
     def __init__(self, filename="movies.json"):
         self.filepath = Path(filename)
+        self._ensure_file_exists()
+
+    #check file exists
+    def _ensure_file_exists(self):
+        if not self.filepath.exists():
+            with open(self.filepath, "w") as file:
+                json.dump({"movies": []}, file)
+    
 
 if __name__ == "__main__":
     repo = MovieRepository()
