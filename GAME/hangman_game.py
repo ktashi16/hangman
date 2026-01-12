a18c15a4410dd04d97e5a63d295ea18374c51c92
karma gurung <ktashi16@gmail.com>
2026-01-12 11:30:49 -0500

    added function that mask the words

--- GAME/hangman_game.py
+++ GAME/hangman_game.py
@@ -3,10 +3,22 @@ import random
 
 class HangmanGame:
     def __init__(self, movies):
-        self.word = random.choice(movies) #selecting a random word from movies.json
+        self.word = random.choice(movies).upper()  #selecting a random word from movies.json
+        self.masked_word = self._mask_word()
+        
+    def _mask_word(self):
+        masked = ""
+        for char in self.word:
+            if char == " ":
+                masked += " "
+            else:
+                masked += "_"
+        return masked
+
 
 
 if __name__ == "__main__":
     movies = ["Inception", "Interstellar"]
     game = HangmanGame(movies)
     print("Chosen word:", game.word)
+    print("Masked:", game.masked_word)
